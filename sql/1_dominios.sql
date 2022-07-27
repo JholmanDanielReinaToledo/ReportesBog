62460929a26f1fc836b9ac02d3c9a71094be84d1
Jholman Daniel Reina Toledo <reina.daniel1005@gmail.com>
2022-07-26 21:46:31 -0500

    Creacion de nuevos dominios para las direcciones y el estado de los reportes

--- sql/1_dominios.sql
+++ sql/1_dominios.sql
@@ -2,3 +2,28 @@ create table dominios.tipo_documento (
   id bigserial primary key,
   descripcion varchar
 );
+
+create table dominios.orientacion (
+  id bigserial primary key,
+  descripcion varchar
+);
+
+create table dominios.nomenclatura_vial (
+  id bigserial primary key,
+  descripcion varchar
+);
+
+create table dominios.estado_reporte (
+  id bigserial primary key,
+  descripcion varchar
+);
+
+create table dominios.barrio (
+  id bigserial primary key,
+  descripcion varchar
+);
+
+create table dominios.localidad (
+  id bigserial primary key,
+  descripcion varchar
+);
